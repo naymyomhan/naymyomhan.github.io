8225abe66b80216b395575c47b8a86204d597378
Nay Myo Mhan <116763630+naymyomhan@users.noreply.github.com>
2023-09-10 18:04:02 +0700

    Add files via upload

---
+++ shankoemee_4d/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "yoyo-shankoemee-1.0";
+const contentToCache = [
+    "Build/shankoemee_4d.loader.js",
+    "Build/shankoemee_4d.framework.js",
+    "Build/shankoemee_4d.data",
+    "Build/shankoemee_4d.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
